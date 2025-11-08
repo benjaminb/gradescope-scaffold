b3463701e7c7d4537eea9ef21d170e46ed005998
benjaminb <benbasseri@gmail.com>
2025-11-07 19:28:35 -0500

    adds common/source and .gitignore

---
+++ common/source/run_tests.py
@@ -0,0 +1,7 @@
+# From Gradescope
+import unittest
+from gradescope_utils.autograder_utils.json_test_runner import JSONTestRunner
+
+if __name__ == '__main__':
+    suite = unittest.defaultTestLoader.discover('tests')
+    JSONTestRunner().run(suite)
